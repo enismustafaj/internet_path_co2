701d05415de14e93ea7d6696ff21e71054b43272
enismustafaj <40141091+enismustafaj@users.noreply.github.com>
2022-04-11 22:18:24 +0200

    feat: Check for command failure

--- main.py
+++ main.py
@@ -10,66 +10,72 @@ import utils
 load_dotenv()
 
 arg_parser = argparse.ArgumentParser(
-    description='A tool to mesasure CO2 impact of internet route')
-arg_parser.add_argument('--source', help='Provide source file of websites')
-arg_parser.add_argument('--command', help='Provide traceroute command')
+    description="A tool to mesasure CO2 impact of internet route"
+)
+arg_parser.add_argument("--source", help="Provide source file of websites")
+arg_parser.add_argument("--command", help="Provide traceroute command")
 
 args = arg_parser.parse_args()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     website_carbon = dict()
 
     if args.command:
-        trace_command = args.command
+        trace_command = args.command.split(" ")
     else:
-        if platform.system() == 'Windows':
-            trace_command = 'tracert'
+        if platform.system() == "Windows":
+            trace_command = ["tracert"]
         else:
-            trace_command = 'traceroute'
+            trace_command = ["traceroute"]
 
     if args.source:
         sites = utils.read_filesites(args.source)
     else:
-        sites = utils.read_filesites('./topsites.txt')
+        sites = utils.read_filesites("./topsites.txt")
 
     for i, site in enumerate(sites):
         if i == 1:
             break
-        print('Tracerouting ', site, ' ...')
-        routes = subprocess.run([trace_command, site],
-                                stdout=subprocess.PIPE,
-                                text=True)
-        hops = utils.parse_output(routes.stdout)
+        print("Tracerouting ", site, " ...")
+        try:
+            routes = subprocess.check_call(
+                trace_command + [site],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            hops = utils.parse_output(routes.stdout)
 
-        geolocations = []
-        for hop in hops:
-            geolocations.append(
-                utils.get_location_from_ip(
-                    constants.IP_DATA_ENDPOINT % hop, None,
-                    {'api-key': os.getenv('IP_DATA_API_KEY')}))
+            geolocations = []
+            for hop in hops:
+                geolocations.append(
+                    utils.get_location_from_ip(
+                        constants.IP_DATA_ENDPOINT % hop,
+                        None,
+                        {"api-key": os.getenv("IP_DATA_API_KEY")},
+                    )
+                )
 
-        carbon_intensities = []
+            carbon_intensities = []
 
-        for i, location in enumerate(geolocations):
-            if not len(location) == 0:
-                try:
-                    carbon_intensities.append(
-                        utils.get_carbon_intensity(
-                            constants.CO2_SIGNAL_ENDPOINT,
-                            headers={
-                                'auth-token': os.getenv('CO2_SIGNAL_API_KEY')
-                            },
-                            params={
-                                'lat': location[0],
-                                'lon': location[1]
-                            }))
-                except utils.APIFailException as e:
-                    print(e)
+            for i, location in enumerate(geolocations):
+                if not len(location) == 0:
+                    try:
+                        carbon_intensities.append(
+                            utils.get_carbon_intensity(
+                                constants.CO2_SIGNAL_ENDPOINT,
+                                headers={"auth-token": os.getenv("CO2_SIGNAL_API_KEY")},
+                                params={"lat": location[0], "lon": location[1]},
+                            )
+                        )
+                    except utils.APIFailException as e:
+                        print(e)
 
-        website_carbon[site] = {
-            'hops': hops,
-            'carbon_intensities': carbon_intensities
-        }
+            website_carbon[site] = {
+                "hops": hops,
+                "carbon_intensities": carbon_intensities,
+            }
 
-    utils.print_results_to_file(website_carbon)
+            utils.print_results_to_file(website_carbon)
+        except subprocess.CalledProcessError as e:
+            print("Error: ", e.returncode)
